fbf1f8edde945cd13c3b9651e30abb78bacebe96
Rituraj-30 <rutu2930@gmail.com>
2026-07-21 16:13:50 +0530

    Time: 0 ms (100%), Space: 18.6 MB (33.65%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,18 +11,35 @@
  */
 class Solution {
 public:
-    bool helper(TreeNode* t1,TreeNode* t2){
-        if(t1==NULL && t2==NULL)return true;
+    // bool helper(TreeNode* t1,TreeNode* t2){
+    //     if(t1==NULL && t2==NULL)return true;
 
-        if(t1==NULL || t2==NULL)return false;
+    //     if(t1==NULL || t2==NULL)return false;
 
-        return (t1->val==t2->val)&& 
-        helper(t1->left,t2->right)
-         && helper(t1->right,t2->left);
+    //     return (t1->val==t2->val)&& 
+    //     helper(t1->left,t2->right)
+    //      && helper(t1->right,t2->left);
 
-    }
+    // }
     bool isSymmetric(TreeNode* root) {
         if (root == nullptr) return true;
-        return helper(root->left,root->right);
+        // return helper(root->left,root->right);
+
+        queue<pair<TreeNode*,TreeNode*>> q;
+        q.push({root->left,root->right});
+
+        while(!q.empty()){
+            auto [t1,t2]=q.front();
+            q.pop();
+
+            if(t1==NULL && t2==NULL)continue;
+            if(t1==NULL || t2==NULL || t1->val  !=t2->val ){
+                return false;
+            }
+
+            q.push({t1->left,t2->right});
+            q.push({t1->right,t2->left});
+        }
+        return true;
     }
 };
