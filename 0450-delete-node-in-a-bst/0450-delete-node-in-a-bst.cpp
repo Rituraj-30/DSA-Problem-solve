8b4ff2ec484d1f0787d43bed77512409cf5b3c80
Rituraj-30 <rutu2930@gmail.com>
2026-07-23 16:51:30 +0530

    Time: 0 ms (100%), Space: 34.5 MB (16.06%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+     TreeNode* findMIN( TreeNode* node){
+        while(node->left!=NULL){
+            node=node->left;
+        }
+
+        return node;
+     }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+
+        if(root==NULL)return NULL;
+
+        if(key<root->val){
+                root->left=deleteNode(root->left,key);
+        }else if(key>root->val){
+                root->right=deleteNode(root->right,key);
+        }
+        else {
+
+
+            if (root->left == nullptr) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp; // Right child parent ke sath attach ho jayega
+            } 
+            else if (root->right == nullptr) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp; // Left child parent ke sath attach ho jayega
+            }
+
+            TreeNode* temp=findMIN(root->right);
+            root->val=temp->val;
+            root->right=deleteNode(root->right,temp->val);
+
+        }
+        return root;
+    }
+};
